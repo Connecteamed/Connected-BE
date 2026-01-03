cf81e5ed07aaf9c1cac80ee689da1a4ae9eb841c
Sejeong Kim <sejeonggim68@gmail.com>
2026-01-03 14:20:17 +0900

    chore: Spring Boot 프로젝트 초기 세팅 (JDK 21)

---
+++ src/main/java/com/connected/be/ConnectedBeApplication.java
@@ -0,0 +1,13 @@
+package com.connected.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectedBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectedBeApplication.class, args);
+	}
+
+}
