072ea3abbca79ed3e2fc5dc000048f96e977b169
fbehdgus <danny90697@gmail.com>
2026-01-12 02:26:21 +0900

    feat: swagger 세팅
    -swagger ui 접속 경로 /docs로 변경

--- src/main/java/com/connected/be/ConnectedBeApplication.java
+++ src/main/java/com/connected/be/ConnectedBeApplication.java
@@ -3,10 +3,12 @@ package com.connected.be;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.connecteamed.server")
+@ComponentScan(basePackages = {"com.connecteamed.server"})
 @EnableJpaAuditing
 public class ConnectedBeApplication {
 
