03dd8b43a3ed4112e296d6139f034cbddb4ac44b
Park Sang Min <sm0104224@gmail.com>
2026-01-14 19:38:38 +0900

    feat(member): projectMember Repository 구현

--- src/main/java/com/connecteamed/server/domain/project/repository/ProjectMemberRepository.java
+++ src/main/java/com/connecteamed/server/domain/project/repository/ProjectMemberRepository.java
@@ -1,9 +1,17 @@
 package com.connecteamed.server.domain.project.repository;
 
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
 import com.connecteamed.server.domain.project.entity.ProjectMember;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
+
+    @EntityGraph(attributePaths = {"member", "roles", "roles.role"})
+    List<ProjectMember> findAllByProjectId(Long projectId);
+
+    @EntityGraph(attributePaths = {"member", "roles", "roles.role"})
     Optional<ProjectMember> findByIdAndProjectId(Long id, Long projectId);
 }
---
+++ src/main/java/com/connecteamed/server/domain/project/repository/ProjectRoleRepository.java
@@ -0,0 +1,7 @@
+package com.connecteamed.server.domain.project.repository;
+
+import com.connecteamed.server.domain.project.entity.ProjectRole;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRoleRepository extends JpaRepository<ProjectRole, Long> {
+}
