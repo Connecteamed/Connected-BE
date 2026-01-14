964c24ff9712bc0a08309f4d56a0c0ce67e9d956
fbehdgus <danny90697@gmail.com>
2026-01-14 18:04:30 +0900

    feat: 인증 관련 예외  처리 정의

---
+++ src/main/java/com/connecteamed/server/global/auth/exception/code1/AuthErrorCode.java
@@ -0,0 +1,23 @@
+package com.connecteamed.server.global.auth.exception.code1;
+
+import com.connecteamed.server.global.apiPayload.code.BaseErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum AuthErrorCode implements BaseErrorCode {
+
+
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED,"MEMBER4011", "비밀번호가 틀렸습니다"),
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH4013", "토큰이 만료되었습니다."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "AUTH4014", "유효하지 않은 토큰입니다."),
+    // 회원가입 시 중복된 아이디가 있을 경우
+    DUPLICATE_LOGIN_ID(HttpStatus.CONFLICT, "AUTH4091", "이미 존재하는 아이디입니다.")
+    ;
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
---
+++ src/main/java/com/connecteamed/server/global/auth/exception/code1/AuthSuccessCode.java
@@ -0,0 +1,17 @@
+package com.connecteamed.server.global.auth.exception.code1;
+
+import com.connecteamed.server.global.apiPayload.code.BaseSuccessCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum AuthSuccessCode implements BaseSuccessCode {
+    LOGIN_SUCCESS(HttpStatus.OK, "MEMBER2001", "로그인에 성공하였습니다."),
+    SIGNUP_SUCCESS(HttpStatus.CREATED, "MEMBER2011", "회원가입이 완료되었습니다.");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+}
