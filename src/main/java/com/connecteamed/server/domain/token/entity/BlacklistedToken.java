0deb358b32c1c03200c582a18428f2ba166c00f7
fbehdgus <danny90697@gmail.com>
2026-01-15 20:49:57 +0900

    feat: 로그아웃 토큰 entity 추가

---
+++ src/main/java/com/connecteamed/server/domain/token/entity/BlacklistedToken.java
@@ -0,0 +1,31 @@
+package com.connecteamed.server.domain.token.entity;
+
+import com.connecteamed.server.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Getter
+@NoArgsConstructor(access= AccessLevel.PROTECTED)
+public class BlacklistedToken extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, updatable = false)
+    private String token;
+
+    @Column(nullable = false, updatable = false)
+    private Instant expiryDate;
+
+    public BlacklistedToken(String token, Instant expiryDate) {
+        this.token = token;
+        this.expiryDate = expiryDate;
+    }
+
+}
