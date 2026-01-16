564a1ce4d4570a9d9bb429046ffcba5cc8850d6c
Park Sang Min <sm0104224@gmail.com>
2026-01-17 00:35:56 +0900

    feat(task): service 로직 구현

--- src/main/java/com/connecteamed/server/domain/task/service/TaskService.java
+++ src/main/java/com/connecteamed/server/domain/task/service/TaskService.java
@@ -1,4 +1,23 @@
 package com.connecteamed.server.domain.task.service;
 
-public class TaskService {
+import com.connecteamed.server.domain.task.dto.*;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface TaskService {
+
+    UUID createTask(Long projectId, TaskCreateReq req);
+
+    List<TaskSummaryRes> getProjectTasks(Long projectId);
+
+    TaskDetailRes getTaskDetail(UUID taskId);
+
+    void updateTaskStatus(UUID taskId, TaskStatusUpdateReq req);
+
+    void updateTaskSchedule(UUID taskId, TaskScheduleUpdateReq req);
+
+    void updateTaskAssignees(UUID taskId, TaskAssigneeUpdateReq req);
+
+    void deleteTask(UUID taskId);
 }
