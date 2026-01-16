b1861c0765f27752a84c154c3b5466ff21aa7636
Park Sang Min <sm0104224@gmail.com>
2026-01-16 14:05:50 +0900

    feat(JapAuditingConfig): 불필요한 설정 수정 JpaAuditingConfig

--- src/main/java/com/connecteamed/server/ConnecteamedApplication.java
+++ src/main/java/com/connecteamed/server/ConnecteamedApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing(dateTimeProviderRef = "dateTimeProvider")
+@EnableJpaAuditing
 public class ConnecteamedApplication {
 
 	public static void main(String[] args) {
