7569a27ed779f8aa9570cd4dfc9a37909643b819
fbehdgus <danny90697@gmail.com>
2026-01-16 11:33:39 +0900

    feat: refreshToken entity 생성

---
+++ src/main/java/com/connecteamed/server/domain/token/repository/RefreshTokenRepository.java
@@ -0,0 +1,17 @@
+package com.connecteamed.server.domain.token.repository;
+
+import com.connecteamed.server.domain.member.entity.Member;
+import com.connecteamed.server.domain.token.entity.RefreshToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends JpaRepository<RefreshToken,Long> {
+    Optional<RefreshToken> findByToken(String refreshToken);
+
+    //기존 토큰 존재 여부 확인
+    Optional<RefreshToken> findByMember(Member member);
+
+    //로그인시 기존 토큰 삭제용
+    void deleteByMember(Member member);
+}
