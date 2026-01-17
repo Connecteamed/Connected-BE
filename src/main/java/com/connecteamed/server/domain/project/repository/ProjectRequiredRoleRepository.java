8998b14984a8b0e672e2ef3e3d4ecd38397749b8
Park Sang Min <sm0104224@gmail.com>
2026-01-17 15:38:34 +0900

    fix(project_repository): merge conflict resolve test3

--- src/main/java/com/connecteamed/server/domain/project/repository/ProjectRequiredRoleRepository.java
+++ src/main/java/com/connecteamed/server/domain/project/repository/ProjectRequiredRoleRepository.java
@@ -1,13 +1,12 @@
 package com.connecteamed.server.domain.project.repository;
 
-import java.util.List;
-
+import com.connecteamed.server.domain.project.entity.ProjectRequiredRole;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.connecteamed.server.domain.project.entity.ProjectRequiredRole;
+import java.util.List;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 @Repository
 public interface ProjectRequiredRoleRepository extends JpaRepository<ProjectRequiredRole, Long> {
