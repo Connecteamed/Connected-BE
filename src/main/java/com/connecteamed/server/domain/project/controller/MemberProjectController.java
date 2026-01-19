9c3141134f965d98726447ed322c97707f2f45f6
fbehdgus <danny90697@gmail.com>
2026-01-20 00:43:47 +0900

    Refactor: 잘못된 url경로 수정

--- src/main/java/com/connecteamed/server/domain/project/controller/MemberProjectController.java
+++ src/main/java/com/connecteamed/server/domain/project/controller/MemberProjectController.java
@@ -18,13 +18,13 @@ public class MemberProjectController {
     private final ProjectService projectService;
 
     @Operation(summary = "내가 완료한 프로젝트 목록 조회", description = "현재 로그인한 사용자가 참여한 프로젝트 중 완료(COMPLETED) 상태인 목록을 조회합니다.")
-    @GetMapping("me/projects/completed")
+    @GetMapping("/me/projects/completed")
     public ApiResponse<ProjectListRes.CompletedProjectList> getCompletedProjects() {
         return ApiResponse.onSuccess(ProjectSuccessCode.OK,projectService.getMyCompletedProjects());
     }
 
     @Operation(summary = "완료한 프로젝트 삭제", description = "특정 프로젝트 이력을 삭제합니다. 프로젝트 소유자(Owner)만 삭제 가능하게 구현했습니다.")
-    @DeleteMapping("me/projects/{projectId}")
+    @DeleteMapping("/me/projects/{projectId}")
     public ApiResponse<Void> deleteProject(@PathVariable Long projectId) {
         projectService.deleteCompletedProject(projectId);
         return ApiResponse.onSuccess(ProjectSuccessCode.OK, null);
