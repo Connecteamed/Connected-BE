bfc58765e18a0688e9e168058cbbdc1be09c91b2
Park Sang Min <sm0104224@gmail.com>
2026-01-21 18:18:01 +0900

    refactor(DocumentController): ApiResponse 반환을 ResponseEntity로 통일

--- src/main/java/com/connecteamed/server/domain/document/controller/DocumentController.java
+++ src/main/java/com/connecteamed/server/domain/document/controller/DocumentController.java
@@ -1,5 +1,7 @@
 package com.connecteamed.server.domain.document.controller;
 
+import java.net.ResponseCache;
+
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -41,14 +43,18 @@ public class DocumentController {
 
     @Operation(summary = "문서 목록 조회", description = "문서 목록의 조회하는 API입니다.")
     @GetMapping("/projects/{projectId}/documents")
-    public ApiResponse<DocumentListRes> list(@PathVariable Long projectId) {
-        return ApiResponse.onSuccess(GeneralSuccessCode._OK, documentService.list(projectId));
+    public ResponseEntity<ApiResponse<DocumentListRes>> list(@PathVariable Long projectId) {
+        return ResponseEntity.ok(
+            ApiResponse.onSuccess(GeneralSuccessCode._OK, documentService.list(projectId))
+        );
     }
 
     @Operation(summary = "문서 상세 조회", description = "문서를 상세를 조회하는 API입니다.")
     @GetMapping("/documents/{documentId}")
-    public ApiResponse<DocumentDetailRes> detail(@PathVariable Long documentId) {
-        return ApiResponse.onSuccess(GeneralSuccessCode._OK, documentService.detail(documentId));
+    public ResponseEntity<ApiResponse<DocumentDetailRes>> detail(@PathVariable Long documentId) {
+        return ResponseEntity.ok(
+            ApiResponse.onSuccess(GeneralSuccessCode._OK, documentService.detail(documentId))
+        );
     }
 
     @Operation(summary = "문서 다운로드", description = "문서 다운로드 API입니다.")
@@ -59,43 +65,51 @@ public class DocumentController {
 
     @Operation(summary = "문서 추가(파일 업로드)", description = "문서 추가 (파일 업로드) API입니다.")
     @PostMapping(value = "/projects/{projectId}/documents/upload", consumes = "multipart/form-data")
-    public ApiResponse<DocumentUploadRes> upload(
+    public ResponseEntity<ApiResponse<DocumentUploadRes>> upload(
             @PathVariable Long projectId,
             @RequestPart("file") MultipartFile file,
             @RequestPart("type") String type
     ) {
         Long projectMemberId = 1L; // TODO 인증에서 꺼내오기
         DocumentFileType fileType = DocumentFileType.valueOf(type);
-        return ApiResponse.onSuccess(GeneralSuccessCode._CREATED, documentService.uploadFile(projectId, projectMemberId, file, fileType));
+        return ResponseEntity.ok(
+            ApiResponse.onSuccess(GeneralSuccessCode._CREATED, documentService.uploadFile(projectId, projectMemberId, file, fileType))
+        );
     }
 
     @Operation(summary = "문서 추가(텍스트 작성)", description = "문서 추가 (텍스트 작성) API입니다.")
     @PostMapping("/projects/{projectId}/documents/text")
-    public ApiResponse<DocumentCreateRes> createText(
+    public ResponseEntity<ApiResponse<DocumentCreateRes>> createText(
             @PathVariable Long projectId,
             @Valid @RequestBody DocumentCreateTextReq req
     ) {
         Long projectMemberId = 1L; // TODO 인증에서 꺼내오기
-        return ApiResponse.onSuccess(GeneralSuccessCode._CREATED, documentService.createText(projectId, projectMemberId, req));
+        return ResponseEntity.ok(
+            ApiResponse.onSuccess(GeneralSuccessCode._CREATED, documentService.createText(projectId, projectMemberId, req))
+        );
     }
 
     @Operation(summary = "문서 수정(텍스트)", description = "문서수정(텍스트) API입니다.")
     @PatchMapping("/documents/{documentId}")
-    public ApiResponse<Void> updateText(
+    public ResponseEntity<ApiResponse<Void>> updateText(
             @PathVariable Long documentId,
             @RequestBody DocumentUpdateTextReq req
     ) {
         Long projectMemberId = 1L; // TODO 인증에서 꺼내오기
         documentService.updateText(documentId, projectMemberId, req);
-        return ApiResponse.onSuccess(GeneralSuccessCode._OK, null);
+        return ResponseEntity.ok(
+            ApiResponse.onSuccess(GeneralSuccessCode._OK, null)
+        );
     }
 
     @Operation(summary = "문서 삭제", description = "문서삭제 API입니다.")
     @DeleteMapping("/documents/{documentId}")
-    public ApiResponse<Void> delete(@PathVariable Long documentId) {
+    public ResponseEntity<ApiResponse<Void>> delete(@PathVariable Long documentId) {
         Long projectMemberId = 1L; // TODO 인증에서 꺼내오기
         documentService.delete(documentId, projectMemberId);
-        return ApiResponse.onSuccess(GeneralSuccessCode._OK, null);
+        return ResponseEntity.ok(
+            ApiResponse.onSuccess(GeneralSuccessCode._OK, null)
+        );
     }
 
 }
