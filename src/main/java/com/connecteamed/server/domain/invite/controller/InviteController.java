ee8aa7cd86285b8946bdf05952dc9fac077f0439
fbehdgus <danny90697@gmail.com>
2026-01-24 00:44:05 +0900

    feat: 초대코드 API 구현

--- src/main/java/com/connecteamed/server/domain/invite/controller/InviteController.java
+++ src/main/java/com/connecteamed/server/domain/invite/controller/InviteController.java
@@ -1,5 +1,52 @@
 package com.connecteamed.server.domain.invite.controller;
 
+
+import com.connecteamed.server.domain.invite.code.InviteSuccessCode;
+import com.connecteamed.server.domain.invite.dto.InviteCodeRes;
+import com.connecteamed.server.domain.invite.dto.ProjectJoinReq;
+import com.connecteamed.server.domain.invite.service.InviteService;
+import com.connecteamed.server.global.apiPayload.ApiResponse;
+import com.connecteamed.server.global.util.SecurityUtil;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "프로젝트 초대 API", description = "프로젝트 초대 코드 발급 및 입장 관련 API")
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/invite")
 public class InviteController {
+
+
+    private final InviteService inviteService;
+
+    @Operation(summary = "초대 코드 발급", description = "해당 프로젝트의 멤버인 경우 초대 코드를 조회하거나 새로 생성합니다.")
+    @GetMapping("/{projectId}")
+    public ApiResponse<InviteCodeRes> getInviteCode(
+            @Parameter(description = "프로젝트 ID", example = "1")
+            @PathVariable(name = "projectId") @Positive Long projectId
+    ) {
+        String loginId = SecurityUtil.getCurrentLoginId();
+        InviteCodeRes response = inviteService.getOrGenerateInviteCode(projectId, loginId);
+        return ApiResponse.onSuccess(InviteSuccessCode.INVITE_CODE_GENERATE_SUCCESS, response);
+    }
+
+    @Operation(summary = "초대 코드로 프로젝트 입장", description = "유효한 초대 코드를 입력하여 프로젝트의 새로운 팀원으로 합류합니다.")
+    @PostMapping("/join")
+    public ApiResponse<String> joinProject(
+            @Valid @RequestBody ProjectJoinReq request
+    ) {
+        String loginId = SecurityUtil.getCurrentLoginId();
+
+        inviteService.joinProjectByCode(request.getInviteCode(), loginId);
+
+        return ApiResponse.onSuccess(InviteSuccessCode.INVITE_OK, null);
+    }
 
 }
--- src/main/java/com/connecteamed/server/domain/invite/repository/InviteCodeRepository.java
+++ src/main/java/com/connecteamed/server/domain/invite/repository/InviteCodeRepository.java
@@ -1,5 +1,20 @@
 package com.connecteamed.server.domain.invite.repository;
 
-public class InviteCodeRepository {
+import com.connecteamed.server.domain.invite.entity.InviteCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.Instant;
+import java.util.Optional;
+
+@Repository
+public interface InviteCodeRepository extends JpaRepository<InviteCode, Long> {
+
+    //해당 프로젝트의 유효한 초대코드가 있는지 확인
+    Optional<InviteCode> findTopByProjectIdAndExpiredAtAfterOrderByCreatedAtDesc(Long projectId, Instant now);
+
+    //입력받은 초대 코드 유효한지 확인
+    Optional<InviteCode> findByCodeAndExpiredAtAfter(String code, Instant now);
+
+    boolean existsByCode(String code);
 }
