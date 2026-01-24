2994d2190fd54fb88deacf3438561676b890cb22
fbehdgus <danny90697@gmail.com>
2026-01-24 12:34:12 +0900

    feat: 스웨거 문서 작성

--- src/main/java/com/connecteamed/server/domain/invite/controller/InviteController.java
+++ src/main/java/com/connecteamed/server/domain/invite/controller/InviteController.java
@@ -9,6 +9,9 @@ import com.connecteamed.server.global.apiPayload.ApiResponse;
 import com.connecteamed.server.global.util.SecurityUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
@@ -27,6 +30,29 @@ public class InviteController {
     private final InviteService inviteService;
 
     @Operation(summary = "초대 코드 발급", description = "해당 프로젝트의 멤버인 경우 초대 코드를 조회하거나 새로 생성합니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "200",
+                    description = "초대 코드 발급 성공",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    name = "조회 성공 예시",
+                                    value = """
+                                            {
+                                              "status": "success",
+                                              "data": {
+                                                "inviteCode": "d699e1eb",
+                                                 "expiredAt": "2026-01-24T15:49:44.062127Z"
+                                              },
+                                              "message": "초대 코드 발급에 성공하였습니다.",
+                                              "code": null
+                                            }
+                                            """
+                            )
+                    )
+            )
+    })
     @GetMapping("/{projectId}")
     public ApiResponse<InviteCodeRes> getInviteCode(
             @Parameter(description = "프로젝트 ID", example = "1")
@@ -38,6 +64,26 @@ public class InviteController {
     }
 
     @Operation(summary = "초대 코드로 프로젝트 입장", description = "유효한 초대 코드를 입력하여 프로젝트의 새로운 팀원으로 합류합니다.")
+    @ApiResponses({
+            @io.swagger.v3.oas.annotations.responses.ApiResponse(
+                    responseCode = "200",
+                    description = "초대 코드 발급 성공",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    name = "조회 성공 예시",
+                                    value = """
+                                            {
+                                              "status": "success",
+                                              "data": null,
+                                              "message": "요청에 성공하였습니다.",
+                                              "code": null
+                                            }
+                                            """
+                            )
+                    )
+            )
+    })
     @PostMapping("/join")
     public ApiResponse<String> joinProject(
             @Valid @RequestBody ProjectJoinReq request
