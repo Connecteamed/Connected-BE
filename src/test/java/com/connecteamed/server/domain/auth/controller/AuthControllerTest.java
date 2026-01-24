f19c26be4192c086495bcce757bcf002f21e547d
fbehdgus <danny90697@gmail.com>
2026-01-24 16:11:39 +0900

    refactor: 하드코딩된 Id값 대신 변수값 비교로 변경

--- src/test/java/com/connecteamed/server/domain/auth/controller/AuthControllerTest.java
+++ src/test/java/com/connecteamed/server/domain/auth/controller/AuthControllerTest.java
@@ -18,7 +18,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -64,7 +63,7 @@ class AuthControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.status").value("success"))
-                .andExpect(jsonPath("$.data.memberId").value(1L))
+                .andExpect(jsonPath("$.data.memberId").value(testMemberId))
                 .andExpect(jsonPath("$.data.accessToken").value("eyJhbGci..."))
                 .andExpect(jsonPath("$.data.refreshToken").value("eyJhbGci..."))
                 .andExpect(jsonPath("$.data.grantType").value("Bearer"))
@@ -166,7 +165,7 @@ class AuthControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.status").value("success"))
-                .andExpect(jsonPath("$.data.memberId").value(1L))
+                .andExpect(jsonPath("$.data.memberId").value(testMemberId))
                 .andExpect(jsonPath("$.data.name").value("testUser"))
                 .andExpect(jsonPath("$.message").value("회원가입이 완료되었습니다."))
                 .andExpect(jsonPath("$.code").value(Matchers.nullValue()));
