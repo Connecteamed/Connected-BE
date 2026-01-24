d3c8f5c4ca6c4e7f73fdaa1ce3ab2683c2b89104
fbehdgus <danny90697@gmail.com>
2026-01-24 12:33:59 +0900

    refactor: dto에 어노테이션 추가

--- src/main/java/com/connecteamed/server/domain/invite/dto/ProjectJoinReq.java
+++ src/main/java/com/connecteamed/server/domain/invite/dto/ProjectJoinReq.java
@@ -1,11 +1,13 @@
 package com.connecteamed.server.domain.invite.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ProjectJoinReq {
     @NotBlank(message = "초대 코드는 필수 입력 항목입니다.")
     private String inviteCode;
