d77ae7ccb90307bb36a166defcf80680ba82bba6
Park Sang Min <sm0104224@gmail.com>
2026-01-27 20:31:15 +0900

    chore(task): unused UUID import 정리

--- src/main/java/com/connecteamed/server/domain/task/service/TaskService.java
+++ src/main/java/com/connecteamed/server/domain/task/service/TaskService.java
@@ -3,7 +3,6 @@ package com.connecteamed.server.domain.task.service;
 import com.connecteamed.server.domain.task.dto.*;
 
 import java.util.List;
-import java.util.UUID;
 
 public interface TaskService {
 
